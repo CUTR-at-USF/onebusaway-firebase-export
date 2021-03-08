c1dfffe3bba56383e72c1aaea6d434eb5e1ff499
Wilson Lozano <welozano@hotmail.com>
2021-03-08 12:58:39 -0500

    feat: Test unit for validateStringDateAndParseToMillis

--- src/test/java/edu/usf/cutr/tba/test/StringUtilsTest.java
+++ src/test/java/edu/usf/cutr/tba/test/StringUtilsTest.java
@@ -3,8 +3,7 @@ package edu.usf.cutr.tba.test;
 import edu.usf.cutr.tba.utils.StringUtils;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Tests utilities for Strings functionalities
@@ -17,35 +16,35 @@ public class StringUtilsTest {
      * verify the behavior of GetClosestDeviceInfo
      */
     @Test
-    public void testValidateStringDateJava8() {
+    public void testValidateStringDateAndParseToMillis() {
         String strDate1 = "02-29-2021";
-        String strDate2 = "02-29-2020";
+        String strDate2 = "02-29-2020"; // 1582952400000L America/New York
         String strDate3 = "02152021";
-        String strDate4 = "02-18-2021";
+        String strDate4 = "02-18-2021"; // 1613624400000L America New York
         String strDate5 = "21-05-2020";
         String strDate6 = "May-05-2020";
         String strDate7 = "Hello World";
         String strDate8 = "123456788";
 
         // Verify for a invalid leap year
-        assertFalse(StringUtils.validateStringDateJava8(strDate1));
+        assertEquals(0, StringUtils.validateStringDateAndParseToMillis(strDate1));
 
         // Verify valid leap year
-        assertTrue(StringUtils.validateStringDateJava8(strDate2));
+        assertEquals(1582952400000L, StringUtils.validateStringDateAndParseToMillis(strDate2));
 
         //Verify invalid format
-        assertFalse(StringUtils.validateStringDateJava8(strDate3));
+        assertEquals(0, StringUtils.validateStringDateAndParseToMillis(strDate3));
 
         //Verify valid date
-        assertTrue(StringUtils.validateStringDateJava8(strDate4));
+        assertEquals(1613624400000L, StringUtils.validateStringDateAndParseToMillis(strDate4));
 
         //Verify valid date, invalid format
-        assertFalse(StringUtils.validateStringDateJava8(strDate5));
-        assertFalse(StringUtils.validateStringDateJava8(strDate6));
+        assertEquals(0, StringUtils.validateStringDateAndParseToMillis(strDate5));
+        assertEquals(0, StringUtils.validateStringDateAndParseToMillis(strDate6));
 
         //Verify invalid strings
-        assertFalse(StringUtils.validateStringDateJava8(strDate7));
-        assertFalse(StringUtils.validateStringDateJava8(strDate8));
+        assertEquals(0, StringUtils.validateStringDateAndParseToMillis(strDate7));
+        assertEquals(0, StringUtils.validateStringDateAndParseToMillis(strDate8));
 
     }
 }
