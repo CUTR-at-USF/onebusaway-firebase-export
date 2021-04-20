1bcac8bfb3ab9f0b61817a28e57a0bc576fbed3a
Wilson Lozano <welozano@hotmail.com>
2021-04-20 09:10:58 -0400

    Add string utils functions to make sure a time stamp is parsable to Long and to parse a timeStamp to long.

--- src/main/java/edu/usf/cutr/tba/utils/StringUtils.java
+++ src/main/java/edu/usf/cutr/tba/utils/StringUtils.java
@@ -15,6 +15,7 @@
  */
 package edu.usf.cutr.tba.utils;
 import edu.usf.cutr.tba.options.ProgramOptions;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
@@ -84,4 +85,50 @@ public class StringUtils {
         }
     }
 
+    /**
+     * If stringTimestamp is parsable to Long then return its long value, it it is not parsable, the function will find
+     * the best timeStamp option between a prefix value of the provided string or the MIN_VALUE of the Long class.
+     * @param timeStamp string containing a possible time stamp
+     * @return Long time stamp
+     */
+    public static long timeStampToLong(String timeStamp) {
+        if (!NumberUtils.isParsable(timeStamp)) {
+            // The timestamp must be a random string with a numeric prefix, get the prefix
+            int indexDash = timeStamp.indexOf("-");
+            if (indexDash == -1) {
+                // Dash not found, return MIN_VALUE
+                return Long.MIN_VALUE;
+            }
+            timeStamp = timeStamp.substring(0 , indexDash);
+            // if prefix is not Parsable, return MIN_VALUE
+            if (!NumberUtils.isParsable(timeStamp)) {
+                return Long.MIN_VALUE;
+            }
+        }
+        // timeStamp is parsable
+        return Long.parseLong(timeStamp);
+    }
+
+    /**
+     * If the provided timeStamp is parsable return the same string. If it is not parsable, the function will find the
+     * best timeStamp option between a prefix value of the provided string or the MIN_VALUE of the Long class.
+     * @param timeStamp string containing a possible time stamp
+     * @return string with a parsable time stamp
+     */
+    public static String parsableTimeStamp(String timeStamp) {
+        if (!NumberUtils.isParsable(timeStamp)) {
+            // The timestamp must be a random string with a numeric prefix, get the prefix
+            int indexDash = timeStamp.indexOf("-");
+            if (indexDash == -1) {
+                // Dash not found, return MIN_VALUE
+                return Long.toString(Long.MIN_VALUE);
+            }
+            timeStamp = timeStamp.substring(0 , indexDash);
+            // if prefix is not Parsable, return MIN_VALUE
+            if (!NumberUtils.isParsable(timeStamp)) {
+                return Long.toString(Long.MIN_VALUE);
+            }
+        }
+        return timeStamp;
+    }
 }
